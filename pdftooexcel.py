e79338a92d0cf684f97dbd4141c02e38e8d90356
SEBIN POULOSE <sebinpoulose99@gmail.com>
2018-11-01 18:24:47 +0530

    Update pdftooexcel.py
    changed the code to only include the semester subjects in the excel file.
    output.csv contains old output and improvised.csv has the new output.

--- pdftooexcel.py
+++ pdftooexcel.py
@@ -3,6 +3,9 @@ import re
 import csv
 import math
 from datetime import datetime
+
+megaarray=[]#to store every row extracted from text file
+
 currentyear = datetime.now().year % 100
 os.system("pdftotext -nopgbrk input1.pdf")
 clgnamefound = 0
@@ -25,6 +28,12 @@ for i in range(0, maxlength):
     elif batchfound == 1 and re.match("L?[A-Z]{3}"+str(batch)+"L?[A-Z]{2}[A-Z]?[0-9]{3}", fileLines[i]):
         regno = fileLines[i]
         marksblock = fileLines[i+2].split(',')
+        if re.match("L?[A-Z]{2}[0-9]{3}",fileLines[i+3]):
+            striplist=[]
+            for i in fileLines[i+3].split(','):
+                striplist.append(i.strip())
+            marksblock = marksblock +striplist
+            striplist=[]
         try:
             marksblock.remove("\n")
         except:
@@ -35,6 +44,7 @@ for i in range(0, maxlength):
             marks.append((i.split('(')[1].replace(")", "")))
         marks[-1] = marks[-1].replace('\n', '')
         row = [regno.strip()]+[collagename.strip()]+[branch.strip()]+marks
+        megaarray.append(row)
         writer.writerows([row])
     elif "B.Tech" in fileLines[i]:
         lines = fileLines[i].split()
@@ -44,4 +54,82 @@ for i in range(0, maxlength):
                 sem = int((math.ceil(sem/2)))
                 batch = currentyear - sem
                 batchfound = 1
+f.close()
+
+br = megaarray[0][2] # init branch
+sublist = {}         # init subjectlists in input file
+studentcount = 0
+semsubject = []     #subjects of surrent sem
+for r in megaarray:
+    if r[2] == br:
+        studentcount += 1
+        i = 3
+        while i < len(r):
+            if r[i] in sublist.keys():
+                sublist[r[i]] = int(sublist.get(r[i]))+1
+            else:
+                sublist[r[i]] = 1
+            i += 2
+    else:
+        offset = studentcount / 3
+        temp = []
+        for key, value in sublist.items():
+            if value > offset:
+                temp.append(key)
+        br = r[2]
+        studentcount = 0
+        sublist = {}
+        semsubject.append(temp)
+offset = studentcount / 3
+temp = []
+for key, value in sublist.items():
+    if value > offset:
+        temp.append(key)
+semsubject.append(temp)
+check=0
+br = megaarray[0][2]
+branchno=0
+sub = semsubject[branchno]
+stuarray= []
+actualarray = []
+for i in megaarray:
+    stuarray = i[:3]
+    if i[2] == br:
+        for elem in sub:
+            check=0
+            stuarray.append(elem)
+            count = 3
+            while count < len(i):
+                if elem.strip() in i[count].strip():
+                    check=1
+                    stuarray.append(i[count+1])
+                    count+=1
+                    break
+                else:
+                    count+=1
+            if check==0:
+                stuarray.append('')
+    else:
+        br = i[2]
+        branchno += 1
+        sub = semsubject[branchno]
+        if i[2] == br:
+            for elem in sub:
+                check=0
+                stuarray.append(elem)
+                count = 3
+                while count < len(i):
+                    if elem.strip() == i[count].strip():
+                        check = 1
+                        stuarray.append(i[count + 1])
+                        count += 2
+                    else:
+                        count += 1
+                if check == 0:
+                    stuarray.append('')
+    actualarray.append(stuarray)
+    stuarray = []
+f = open("improvised.csv", "wb")
+writer = csv.writer(f)
+writer.writerows(actualarray)
 f.close()
